917c4855cf1bfa335eee13f3ce9df2eed6897635
vagrant <nihar.patil98@gmail.com>
2016-12-14 07:59:33 +0000

    arrangment email completed

--- app/Http/Controllers/ArrangementMailController.php
+++ app/Http/Controllers/ArrangementMailController.php
@@ -3,12 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Member as Member;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\ShowAssignment as ShowAssignment;
 
 class ArrangementMailController extends Controller
 {
     public static function sendArrangements($arrangements){
     	$members = $arrangements['members'];
     	$assigned = $arrangements['assigned'];
-    	
+
+    	for($i = 0; $i < sizeof($members); $i++){
+    		$member = Member::find($members[$i]);
+    		$assigned = Member::find($assigned[$i]);
+    		$group_name = $member->group()->first()->group_name;
+			Mail::to($member)->send(new ShowAssignment($assigned->name, $group_name));
+    	}
     }
 }
---
+++ resources/views/emails/show_assignment.blade.php
@@ -0,0 +1,4 @@
+<div>
+	<h4>Your assignment is out!</h4>
+	<p>Your assignment for {{$secret_santa_group_name}} is: {{$assignent_name}}</p>
+</div>
